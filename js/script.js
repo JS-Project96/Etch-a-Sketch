c835d45031b35c29486c81f47810834aba924949
JS-Project96 <jshorter206854@gmail.com>
2023-08-03 13:02:28 +0100

    Tidied createGrid func up by moving styling to CSS file

--- js/script.js
+++ js/script.js
@@ -2,13 +2,11 @@ const container = document.querySelector('.container');
 const createGridButton = document.querySelector('.create-grid');
 
 function createGrid() {
-    let number = prompt("Enter a number x to create an x by x grid:");
+    let number = prompt("Enter a number X to create an X by X grid:");
     gridNumber = number * number;
     while (gridNumber > 0) {
         let div = document.createElement('div');
-        div.style.borderStyle = 'solid';
-        div.style.borderWidth = '1px';
-        div.style.margin = '-1px';
+        div.classList.add('grid');
         div.style.flexBasis = `${100/number}%`;
         container.appendChild(div);
         gridNumber--;
