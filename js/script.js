25563ad39316bcf22266d2b84c320452a8e8e9b0
JS-Project96 <jshorter206854@gmail.com>
2023-08-03 12:46:03 +0100

    edited container style, and createGrid func

--- js/script.js
+++ js/script.js
@@ -2,15 +2,15 @@ const container = document.querySelector('.container');
 const createGridButton = document.querySelector('.create-grid');
 
 function createGrid() {
-    let number = prompt("Enter a number x to create and x by x grid:")
-    while (number > 0) {
+    let number = prompt("Enter a number x to create an x by x grid:");
+    gridNumber = number * number;
+    while (gridNumber > 0) {
         let div = document.createElement('div');
         div.style.borderStyle = 'solid';
-        div.style.borderWidth = '2px 1px';
-        div.style.flex = '1';
-        div.style.padding = '25px';
+        div.style.borderWidth = '1px';
+        div.style.flexBasis = `${100/number}%`;
         container.appendChild(div);
-        number--;
+        gridNumber--;
     };
 };
 
